35594755e067a77f6a8b83a613396462c30582df
mosses608 <dungumalomohammed@gmail.com>
2025-07-04 09:38:40 +0300

    new updates nn

--- app/resources/views/templates/profile.blade.php
+++ app/resources/views/templates/profile.blade.php
@@ -28,55 +28,65 @@
                                             </li>
                                             <li class="list-group-item">
                                                 <strong>Registered At:</strong>
-                                                {{ \Carbon\Carbon::parse($resident->created_at)->format('M d, Y') ?? '' }}
+                                                @if ($$resident->created_at)
+                                                    {{ \Carbon\Carbon::parse($resident->created_at)->format('M d, Y') ?? '' }}
+                                                @endif
                                             </li>
                                             <li class="list-group-item">
                                                 <strong>Updated At:</strong>
-                                                {{ \Carbon\Carbon::parse($resident->updated_at)->format('M d, Y') ?? '' }}
+                                                @if ($$resident->updated_at)
+                                                    {{ \Carbon\Carbon::parse($resident->updated_at)->format('M d, Y') ?? '' }}
+                                                @endif
                                             </li>
                                         </ul>
                                     </div>
                                     <div class="col-8">
-                                        <form action="{{ route('update.profile') }}" method="POST" enctype="multipart/form-data">
+                                        <form action="{{ route('update.profile') }}" method="POST"
+                                            enctype="multipart/form-data">
                                             @csrf
                                             @method('PUT')
                                             <div class="row">
                                                 @php
-                                                    $encryptedId = \Illuminate\Support\Facades\Crypt::encrypt($resident->id);
+                                                    $encryptedId = \Illuminate\Support\Facades\Crypt::encrypt(
+                                                        $resident->id,
+                                                    );
                                                 @endphp
-                                                <input type="hidden" name="user_id" id="" value="{{ $encryptedId }}">
+                                                <input type="hidden" name="user_id" id=""
+                                                    value="{{ $encryptedId }}">
                                                 <div class="col-6">
                                                     <label for="" class="form-label">Names
-                                                        </label>
-                                                    <input type="text" name="name" id="location"
-                                                        class="form-control" value="{{ $resident->name ?? '' }}">
+                                                    </label>
+                                                    <input type="text" name="name" id="location" class="form-control"
+                                                        value="{{ $resident->name ?? '' }}">
                                                 </div>
                                                 <div class="col-6">
                                                     <label for="" class="form-label">Phone
-                                                        </label>
+                                                    </label>
                                                     <input type="text" name="phone" id="location" maxlength="10"
-                                                        class="form-control" value="{{ $resident->phone ?? '07xxxxxxxx' }}">
+                                                        class="form-control"
+                                                        value="{{ $resident->phone ?? '07xxxxxxxx' }}">
                                                 </div>
                                                 <div class="col-6 mt-3">
                                                     <label for="" class="form-label">Address
-                                                        </label>
-                                                    <input type="text" name="address" id="address"
-                                                        class="form-control" value="{{ $resident->address ?? 'xxxxxxxx' }}">
+                                                    </label>
+                                                    <input type="text" name="address" id="address" class="form-control"
+                                                        value="{{ $resident->address ?? 'xxxxxxxx' }}">
                                                 </div>
                                                 <div class="col-6 mt-3">
                                                     <label for="" class="form-label">Email
-                                                        </label>
-                                                    <input type="email" name="email" id="email"
-                                                        class="form-control" value="{{ $resident->email ?? 'xxxxxxxx@gmail.com' }}">
+                                                    </label>
+                                                    <input type="email" name="email" id="email" class="form-control"
+                                                        value="{{ $resident->email ?? 'xxxxxxxx@gmail.com' }}">
                                                 </div>
                                                 <div class="col-12 mt-3">
                                                     <label for="" class="form-label">Password
-                                                        </label>
+                                                    </label>
                                                     <input type="password" name="password" id="password"
-                                                        class="form-control" placeholder="enter new password password" disabled>
+                                                        class="form-control" placeholder="enter new password password"
+                                                        disabled>
                                                 </div>
                                                 <div class="col-6 mt-3">
-                                                   <button type="submit" class="btn btn-primary">Update Profile</button>
+                                                    <button type="submit" class="btn btn-primary">Update Profile</button>
                                                 </div>
                                             </div>
                                         </form>
